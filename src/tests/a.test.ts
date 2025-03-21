c6b89be29972f900923f51597a459679f965d326
ClemZi02 <clement.foulon19@gmail.com>
2025-03-21 21:21:41 +0100

    feat: [AIC-02-05] - Implement ESLint and SonarCloud

---
+++ src/tests/a.test.ts
@@ -0,0 +1,9 @@
+// if i don't create this file i can't push the repository "tests"
+
+//Small test to complete the CI with jtest
+//Can't pass the CI without a test
+test("sum of 1 + 2", () => {
+  expect(1 + 2).toBe(3); 
+});
+
+// Think to delete this file when the branch is merged !!!!!!
