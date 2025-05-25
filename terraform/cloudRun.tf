968c4dc4d150ba1cd8b7de75cdef29a7e595e51d
Maxcwt <148951047+Maxcwt@users.noreply.github.com>
2025-05-25 16:45:25 +0200

    Add Terraform configuration for Cloud Run service and IAM invokers

---
+++ terraform/cloudRun.tf
@@ -0,0 +1,25 @@
+module "cloud_run_wealthmanagement-service" {
+  source                = "./modules/cloud_run"
+  project_id            = "intricate-pad-455413-f7"
+  region                = "europe-west1"
+  service_name          = "wealthmanagement-service"
+  repository_id         = "locaccm-repo-docker"
+  service_account_email = module.service_account_wealthmanagement-service.email
+  vpc_connector         = module.vpc_connector.id
+  public                = false
+
+  env_variables = {
+    NODE_ENV = "production"
+  }
+}
+
+module "cloud_run_wealthmanagement-service_invokers" {
+  depends_on = [module.cloud_run_wealthmanagement-service]
+  source        = "./modules/cloud_run_invoker"
+  region        = "europe-west1"
+  service_name  = "wealthmanagement-service"
+  invokers = {
+    frontend            = "frontend-service@intricate-pad-455413-f7.iam.gserviceaccount.com"
+    authentification    = "auth-service@intricate-pad-455413-f7.iam.gserviceaccount.com"
+  }
+}
---
+++ terraform/serviceAccount.tf
@@ -0,0 +1,10 @@
+module "service_account_wealthmanagement-service" {
+  source       = "./modules/service_account"
+  account_id   = "wealthmanagement-service"
+  display_name = "Wealth Management Service Account"
+  project_id   = "intricate-pad-455413-f7"
+  roles        = [
+    "roles/cloudsql.client",
+    "roles/storage.objectViewer"
+  ]
+}
